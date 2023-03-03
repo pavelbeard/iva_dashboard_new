191862515a9ef3d45f1a5462e5ffa5a46a23334d
pavelbeard <pavelborodin1995@gmail.com>
2023-03-04 00:31:32 +0300

    v0.9.29 IvaDashboard v0.9.21 IvaDashboardAgent
    SAVE! Unstable!
    
    Готовы все чарты. Осталось написать логику фильтрации. Также в чартах есть баг, который затирает лейблы
    
    PROD.!!!
    
    Поправил вывод NetInterface
    Поправил чуток в админке.
    Сама админка что-то стала лагать
    
    Обновил команды

--- monitor_serv/dashboard/management/commands/addscrapecommands.py
+++ monitor_serv/dashboard/management/commands/addscrapecommands.py
@@ -15,20 +15,18 @@ class Command(BaseCommand):
         try:
             query = models.ScrapeCommand.objects.create(
                 record_id=0,
-                scrape_command="sudo /usr/sbin/crm status",
-                scrape_command_cpu='top -bn 1 -d.2 | grep "Cpu" && top 1 -w 70 -bn 1 | grep -P "^(%)"',
-                scrape_command_ram="free -b",
-                scrape_command_fs='df && lsblk  -b| grep -E "^sda"',
-                scrape_command_apps="/usr/sbin/service --status-all",
-                scrape_command_net="/usr/sbin/ifconfig",
-                scrape_command_uptime="uptime",
-                scrape_command_hostnamectl="uname -n && uname -r && cat /etc/os-release",
-                scrape_command_load_average="uptime"
+                scrape_command={"crm": 'sudo /usr/sbin/crm status',
+                                "cpu": 'top -bn 1 -d.2 | grep "Cpu" && top 1 -w 70 -bn 1 | grep -P "^(%)"',
+                                "ram": 'free -b',
+                                "fs": 'df && lsblk  -b| grep -E "^sda"',
+                                "apps": '/usr/sbin/service --status-all',
+                                "net": '/usr/sbin/ifconfig',
+                                "server_data": 'uname -n && uname -r && cat /etc/os-release',
+                                "uptime": 'uptime',
+                                "load_average": 'uptime'}
             )
             query.save()
             self.stdout.write(self.style.SUCCESS("Monitoring commands are added to database"))
         except Exception:
             raise CommandError("Scrape commands are added to database.")
             # self.stdout.write(self.style.WARNING())
-
-
